7e6928cd732b676dfba2cb58af270f9ae36d8fec
Taranpal Singh <mantajbrar007@gmail.com>
2026-02-03 12:21:16 +0530

    Updated

--- main.py
+++ main.py
@@ -22,7 +22,11 @@ with col1:
     batting_team=st.selectbox('Select the batting team ',sorted(teams))
 
 with col2:
-    bowling_team=st.selectbox('Select the bowling team ',sorted(teams))
+    bowling_team = st.selectbox(
+        'Select the bowling team',
+        sorted([team for team in teams if team != batting_team])
+    )
+
 
 target=st.number_input('Target')
 col3,col4,col5=st.columns(3)
@@ -37,7 +41,7 @@ if(st.button('Predict Probability')):
     runs_left=target-score
     balls_left=120-(overs*6)
     wickets=10-wickets
-    crr=score/overs
+    crr = score / overs if overs > 0 else 0
     rrr=runs_left*6/balls_left
 
     input=pd.DataFrame({'batting_team':[batting_team],'bowling_team':[bowling_team],'runs_left':[runs_left],'balls_left':[balls_left],'wickets':[wickets],'total_runs_x':[target],'crr':[crr],'rrr':[rrr]})
